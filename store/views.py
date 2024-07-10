fb914880e2aa1ff3a87c9e7a6c9732360f9040f4
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-07-10 18:49:42 +0600

    Add cart and its templates

--- store/views.py
+++ store/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-from .models import (
-Category,
-Product,
-Order,
-Customer
-)
+from django.shortcuts import render, redirect
+from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
+
+
+def category(request, foo):
+    # Replace hyphens with spaces
+    foo = foo.replace('-', '')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category})
+
+    except:
+        messages.error(request, "That category does not exist")
+        return redirect('home')
+
+
+def product(request,pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
 
 
 def home(request):
@@ -13,4 +32,49 @@ def home(request):
 
 
 def about(request):
-    return render(request, 'about.html',)
+    return render(request, 'about.html')
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have been logged in!")
+                return redirect('home')
+            else:
+                messages.error(request, "There was an error, Please try again")
+                return redirect('login')
+        else:
+            messages.error(request, "Username and password are required")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            # login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You Have Registered successfully! Welcome!"))
+            return redirect('home')
+        else:
+            messages.error(request, "Whoops! There was a problem Registering, please try again!")
+            return redirect('register')
+    return render(request, 'register.html', {'form': form})
