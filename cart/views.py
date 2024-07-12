0ea648d1ad583c5184bad550de10723c738c6a62
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-07-12 19:20:18 +0600

    Add cart summary html

--- cart/views.py
+++ cart/views.py
@@ -11,15 +11,49 @@ def cart_summary(request):
     cart_products = cart.get_prods
     quantities = cart.get_quants
     totals = cart.cart_total()
-    return render(request, "cart_summary.html", {})
+    return render(request, "cart_summary.html", {"cart_products": cart_products, "quantities": quantities})
 
 
-# def cart_add(request):
-#     pass
+def cart_add(request):
+    # Get the cart
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        # lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+
+        # Save to session
+        cart.add(product=product, quantity=product_qty)
+
+        # Get Cart Quantity
+        cart_quantity = cart.__len__()
+
+        # Return response
+        # response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, ("Product Added To Cart..."))
+        return response
 
 # def cart_delete(request):
 #     pass
 #
 #
-# def cart_update(request):
-#     pass
+def cart_update(request):
+    # Get the cart
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty': product_qty})
+        return response
+        # return redirect('cart_summary')
+
+
