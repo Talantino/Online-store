f6f0e4840d383ae2a07a40685e4c10f822460b86
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-07-23 12:43:35 +0600

    Add admin, forms and urls for store app

--- store/admin.py
+++ store/admin.py
@@ -3,10 +3,29 @@ from .models import (
     Category,
     Product,
     Customer,
-    Order
+    Order,
+    Profile
 )
+from django.contrib.auth.models import User
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+
+
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re-Register the new way
+admin.site.register(User, UserAdmin)
